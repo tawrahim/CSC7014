d73c274770968e0f1d4edc3d72413bc5780b5c0b
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2015-02-15 16:23:35 -0500

    Finish hw3

--- week3/hw3.py
+++ week3/hw3.py
@@ -10,33 +10,25 @@
 import turtle
 import numbers
 
+# This dictionary contains the pattern of each digit
+# A 100 signifies a tall bar whiles a 50 represents
+# a short bar
 map_of_postal_codes =  {
-    0: [200, 200, 100, 100, 100],
-    1: [100, 100, 100, 200, 200],
-    2: [100, 100, 200, 100, 200],
-    3: [100, 100, 200, 200, 100],
-    4: [100, 200, 100, 100, 200],
-    5: [100, 200, 100, 200, 100],
-    6: [100, 200, 200, 100, 100],
-    7: [200, 100, 100, 100, 200],
-    8: [200, 100, 100, 200, 100],
-    9: [200, 200, 100, 100, 100]
+    0: [100, 100, 50, 50, 50],
+    1: [50, 50, 50, 100, 100],
+    2: [50, 50, 100, 50, 100],
+    3: [50, 50, 100, 100, 50],
+    4: [50, 100, 50, 50, 100],
+    5: [50, 100, 50, 100, 50],
+    6: [50, 100, 100, 50, 50],
+    7: [100, 50, 50, 50, 100],
+    8: [100, 50, 50, 100, 50],
+    9: [100, 100, 50, 50, 50]
 }
-
-def draw_postal_code():
-    # we are not doing any error checking as we assume that the user would enter valid data
-    user_input = raw_input("Please enter a zip code in the format XXXXX-XXXX and it would drawn for you: ")
-
-    for x in user_input:
-        try:
-            value_of_x = int(x)
-            print(map_of_postal_codes[value_of_x])
-        except:
-            print(False)
 
 def drawBar(t, height):
     """ Get turtle t to draw one bar, of height. """
-    t.begin_fill()               # start filling this shape
+    t.begin_fill()             
     t.left(90)
     t.forward(height)
     t.right(90)
@@ -44,36 +36,49 @@ def drawBar(t, height):
     t.right(90)
     t.forward(height)
     t.left(90)
-    t.end_fill()                 # stop filling this shape
+    t.end_fill()
+    t.forward(70)           
 
-def fillColor(t,height):
-    drawBar(t,height)
-    if height >=200:
-        t.fillcolor("red")
-    elif height >= 100 and height < 200:
-        t.fillcolor("yellow")
-    else:
-        t.fillcolor("green")
+def draw_postal_code():
+    # Ask the user for postal code input
+    # we are not doing rigrous error checking as we assume that the user would enter valid data
+    user_input = input("Please enter a zip code in the format XXXXX-XXXX and it would drawn for you: ")
 
-xs = [48,117,200,700,220, 48,117,200,240,220, 48,117,200,240,220, 48,117,200,240,220, 48,117,200,240,220]  # here is the data
-maxheight = max(xs)
-numbars = len(xs)
-border = 5
+    maxheight = 500
+    numbars = 150
+    border = 5
+    running_sum = 0
+    tess = turtle.Turtle()
+    wn = turtle.Screen() 
+    wn.setworldcoordinates(0-border,0-border,40*numbars+border,maxheight+border)
+    
+    # draw the initial frame bar
+    drawBar(tess, 100)
+    
+    for x in user_input:
+        try:
+            value_of_x = int(x)
+            running_sum += value_of_x
+            for i in map_of_postal_codes[value_of_x]:
+                drawBar(tess,i)
+        except:
+            pass
+    
+    # calculate the check digit
+    remainder = running_sum % 10
+    
+    # draw the check digit
+    if remainder != 0:
+        #print("Check digit is " + str(10 - remainder))
+        for x in map_of_postal_codes[10 - remainder]:
+            drawBar(tess, x)
+        
+        
+    # draw the last frame bar
+    drawBar(tess, 100)
+    
+    wn.exitonclick()
 
-'''
-tess = turtle.Turtle()           # create tess and set some attributes
-
-wn = turtle.Screen()             # Set up the window and its attributes
-wn.setworldcoordinates(0-border,0-border,40*numbars+border,maxheight+border)
-
-screen = turtle.Screen()
-move_factor = 2
-for i in xs:
-    fillColor(tess,i)
-
-
-wn.exitonclick()
-'''
 # Entry point of the program
 if __name__ == "__main__":
     draw_postal_code()
